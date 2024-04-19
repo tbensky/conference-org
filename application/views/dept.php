0f96a3a6140bb9f4c08619967518bdd026b7d36c
Tom Bensky <tbensky@gmail.com>
2024-04-19 09:53:16 +0930

    srcf support counting

--- application/views/dept.php
+++ application/views/dept.php
@@ -10,23 +10,24 @@ echo "<h3>Poster submissions by department</h3>";
 $q = $this->db->query("select people from entry where year=? and format='poster'",$year);
 
 $dist = [];
+$santa_rosa = 0;
 foreach($q->result_array() as $row)
 {
     $json = json_decode($row['people'],true);
     $one = false;
     foreach($json as $p)
     {
-       
         if ($p['role'] == "Faculty" and $one == false)
             {
                 $dept = $p['affiliation'];
-                //if ($dept != "Other..." && $dept != '--Select--')
-                {
-                    if (empty($dist[$dept]))
-                        $dist[$dept] = 1;
-                    else $dist[$dept]++;
-                    $one = true;
-                }
+                if (empty($dist[$dept]))
+                    $dist[$dept] = 1;
+                else $dist[$dept]++;
+                $one = true;
+
+                if ($p['santa_rosa'] == 'yes')
+                    $santa_rosa++;
+    
             }
     }
 }
@@ -40,6 +41,8 @@ foreach($dist as $dept => $count)
 }
 echo "</ul>";
 echo "Total: $t";
+echo "<br/>";
+echo "Total noting Santa Rosa Creek Foundation support: $santa_rosa";
 
 echo "<hr/>";
 
